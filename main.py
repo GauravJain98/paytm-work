92f6f8a3dd14eac4fd985bbd2bb9ecf9a673749c
Gaurav Jain <crazcuber@gmail.com>
2018-04-22 22:36:21 +0530

    about us added

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-# @ signifies a decorator - way to wrap a function and modifying its behavior
 @app.route('/')
 def index():
-    return render_template("profile.html", name=name)
+    return render_template("about.html")
+
+@app.route('/privacy')
+def privacy():
+    return render_template("privacy.html")
+
+@app.route('/refund')
+def refund():
+    return render_template("refund.html")
+
+@app.route('/terms')
+def terms():
+    return render_template("terms.html")
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template("contact_us.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
