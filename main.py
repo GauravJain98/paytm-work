6ccc2bd3f6a037e3f6cb748944ad6198e104fcdf
Gaurav Jain <crazcuber@gmail.com>
2018-04-23 01:03:48 +0530

    added contact

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def refund():
 def terms():
     return render_template("terms.html")
 
-@app.route('/contact_us')
+@app.route('/contact')
 def contact_us():
-    return render_template("contact_us.html")
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
